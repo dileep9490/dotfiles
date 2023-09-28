f23a6af782844e88a8a1a3692c4d61d52707ba4f
dileep9490 <dileepgudimetla9490@gmail.com>
2023-09-28 21:49:50 +0530

    initial commit

---
+++ nvim/lua/dileep/plugins/comment.lua
@@ -0,0 +1,15 @@
+return {
+    {
+        'numToStr/Comment.nvim',
+        opts = {
+            -- add any options here
+        },
+        lazy = false,
+    },
+    {
+        "JoosepAlviste/nvim-ts-context-commentstring"
+    }, {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+}
+}
